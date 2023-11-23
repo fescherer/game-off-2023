7e76adbc52c0fe81ec3440d5496513474abb99c0
Felipe Scherer <felipescherer25@gmail.com>
2023-11-23 19:56:28 -0300

    :sparkles: add range indicator

--- Scenes/MainScenes/UI.gd
+++ Scenes/MainScenes/UI.gd
@@ -6,8 +6,18 @@ func set_tower_preview(tower_type, mouse_position):
 	drag_tower.set_name("DragTower")
 	drag_tower.modulate = Color("bb243dd9")
 	
+	var range_texture = Sprite2D.new()
+	range_texture.position = Vector2(0,0)
+	var scaling = GameData.tower_data[tower_type]["range"] / 600.0
+	range_texture.scale = Vector2(scaling, scaling)
+	var texture = load("res://Assets/UI/range_overlay.png")
+	range_texture.texture = texture
+	range_texture.modulate = Color("bb243dd9")
+	range_texture.set_name("RangeIndicator")
+	
 	var control = Control.new()
 	control.add_child(drag_tower, true)
+	control.add_child(range_texture, true)
 	control.position = mouse_position
 	control.set_name("TowerPreview")
 	add_child(control,true)
@@ -17,3 +27,4 @@ func update_tower_preview(new_position, color):
 	get_node("TowerPreview").position = new_position
 	if get_node("TowerPreview/DragTower").modulate != Color(color):
 		get_node("TowerPreview/DragTower").modulate = Color(color)
+		get_node("TowerPreview/RangeIndicator").modulate = Color(color)
---
+++ Singletons/GameData.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var tower_data = {
+	"tower_knight": {
+		"damage": 20,
+		"rof": 1,
+		"range": 48,
+	},
+	"tower_mage": {
+		"damage": 20,
+		"rof": 1,
+		"range": 128,
+	}
+}
